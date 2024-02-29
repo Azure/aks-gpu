ae6dca2297a93cdf71b6f79c16b7fdbe2897412e
Alison Burgess <alburgess@microsoft.com>
2024-02-29 10:59:26 -0800

    chore: remove the call

--- install.sh
+++ install.sh
@@ -19,8 +19,6 @@ echo "Open gridd: $open_gridd"
 
 set -euo pipefail
 
-use_package_manager_with_retries wait_for_apt_locks 10 3
-
 # install cached nvidia debian packages for container runtime compatibility
 install_cached_nvidia_packages() {
 for apt_package in $NVIDIA_PACKAGES; do
