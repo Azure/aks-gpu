10a8ba70781582917c42665288d84dc6ecd1b6e8
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2024-07-26 20:41:06 -0700

    remove nvidia-docker and update deps in download.sh

--- download.sh
+++ download.sh
@@ -41,10 +41,10 @@ fi
 
 
 # configure nvidia apt repo to cache packages
-curl -fsSLO https://nvidia.github.io/nvidia-docker/gpgkey
+curl -fsSLO https://nvidia.github.io/libnvidia-container/gpgkey
 gpg --dearmor -o aptnvidia.gpg gpgkey
 mv aptnvidia.gpg /etc/apt/trusted.gpg.d/aptnvidia.gpg
-curl -fsSL https://nvidia.github.io/nvidia-docker/ubuntu${VERSION_ID}/nvidia-docker.list -o /etc/apt/sources.list.d/nvidia-docker.list
+curl -fsSL https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list -o /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
 apt update
 
@@ -53,17 +53,17 @@ for apt_package in $NVIDIA_PACKAGES; do
     apt-get download ${apt_package}=${NVIDIA_CONTAINER_TOOLKIT_VER}*
     mv ${apt_package}_${NVIDIA_CONTAINER_TOOLKIT_VER}* /opt/gpu
 done
-apt-get download nvidia-container-runtime=${NVIDIA_CONTAINER_RUNTIME_VERSION}*
+#apt-get download nvidia-container-runtime=${NVIDIA_CONTAINER_RUNTIME_VERSION}*
 
 # move debs to permanent cache
-mv nvidia-container-runtime_${NVIDIA_CONTAINER_RUNTIME_VERSION}* /opt/gpu
+#mv nvidia-container-runtime_${NVIDIA_CONTAINER_RUNTIME_VERSION}* /opt/gpu
 
 # nvidia-docker2 for docker runtime
-apt-get download nvidia-docker2=${NVIDIA_DOCKER_VERSION}
-mkdir -p /tmp/nvidia-docker2
-dpkg-deb -R ./nvidia-docker2_${NVIDIA_DOCKER_VERSION}_all.deb /tmp/nvidia-docker2
-mkdir -p /opt/gpu/nvidia-docker2_${NVIDIA_DOCKER_VERSION}
-cp -r /tmp/nvidia-docker2/usr/* /opt/gpu/nvidia-docker2_${NVIDIA_DOCKER_VERSION}/
+# apt-get download nvidia-docker2=${NVIDIA_DOCKER_VERSION}
+# mkdir -p /tmp/nvidia-docker2
+# dpkg-deb -R ./nvidia-docker2_${NVIDIA_DOCKER_VERSION}_all.deb /tmp/nvidia-docker2
+# mkdir -p /opt/gpu/nvidia-docker2_${NVIDIA_DOCKER_VERSION}
+# cp -r /tmp/nvidia-docker2/usr/* /opt/gpu/nvidia-docker2_${NVIDIA_DOCKER_VERSION}/
 
 popd || exit
 rm -r "$workdir"
