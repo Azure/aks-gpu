f58d7590d7ec9369bbae04cd1916e7ed3e1014fa
Ganeshkumar Ashokavardhanan <35557827+ganeshkumarashok@users.noreply.github.com>
2024-10-23 17:32:51 -0700

    Removing unused Nvidia container toolkit ver variable (#67)

--- config.sh
+++ config.sh
@@ -1,6 +1,5 @@
 DRIVER_VERSION="${DRIVER_VERSION}"
 DRIVER_KIND="${DRIVER_KIND}"
-NVIDIA_CONTAINER_RUNTIME_VERSION="3.13.0"
 NVIDIA_CONTAINER_TOOLKIT_VER="1.16.2"
 NVIDIA_PACKAGES="libnvidia-container1 libnvidia-container-tools nvidia-container-toolkit-base nvidia-container-toolkit"
 GPU_DEST="/usr/local/nvidia"
